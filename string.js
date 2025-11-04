f29f35b67428399aeadafe360360e5b9473816d4
alamin-dev0 <alamin.dev01@gmail.com>
2025-11-04 20:34:39 +0600

    added some code

---
+++ string.js
@@ -0,0 +1,22 @@
+const country = 'Bangladesh';
+const division = 'NewKhale';
+const district = `B-Baria`
+// const thana = new String('Demra')
+
+// console.log(thana)
+
+
+const numbers = [54, 98, 78, 21, 22]
+// console.log(numbers.length)
+console.log(numbers)
+numbers[1] = 12;
+console.log(numbers)
+
+
+const capital = 'Dhaka'
+console.log(capital.length)
+console.log(capital[2])
+
+console.log(capital)
+capital[1] = 'q';
+console.log(capital)
