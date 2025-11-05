d51fddf326bcd4500b98ca095c091f2e3b407b9e
alamin-dev0 <alamin.dev01@gmail.com>
2025-11-05 20:43:11 +0600

    added some code

---
+++ reverse.js
@@ -0,0 +1,10 @@
+const sectence = 'I am learning we Dev.'
+
+// const result = '.veD beW gninrel ma I'
+
+
+let reverse = ''
+for (const letter of sectence){
+    reverse = letter + reverse
+}
+console.log(reverse)
